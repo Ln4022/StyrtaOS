2e1e4e31905879ff4537b63cbed420bae113724c
Ln4022 <63056642+Ln4022@users.noreply.github.com>
2026-02-21 17:28:22 +0100

    Upgrade installer from v1.0 to v1.1

--- installer.lua
+++ installer.lua
@@ -1,4 +1,4 @@
--- STYRTA OS Installer v1.0
+-- STYRTA OS Installer v1.1
 
 local user = "Ln4022"
 local repo = "StyrtaOS"
@@ -10,6 +10,7 @@ local files = {
     "startup",
     "system/core.lua",
     "apps/launcher.lua",
+    "apps/sms.lua",
     "update.lua",
     "version.txt"
 }
@@ -24,6 +25,11 @@ if not fs.exists("apps") then fs.makeDir("apps") end
 -- Pobieranie plikow
 for i,file in ipairs(files) do
     print("Pobieranie: "..file)
+
+    if fs.exists(file) then
+        fs.delete(file)
+    end
+
     shell.run("wget", baseURL..file, file)
 end
 
