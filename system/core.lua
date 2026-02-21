eca0212d590c6bd0fee705fb26c969994431f2ab
Ln4022 <63056642+Ln4022@users.noreply.github.com>
2026-02-21 13:47:10 +0100

    Update welcome message to include username

--- system/core.lua
+++ system/core.lua
@@ -1,12 +1,20 @@
--- STYRTA OS Core v1.0
+-- STYRTA OS Core
+
+local file = fs.open("system/config","r")
+local username = file.readLine()
+local pin = file.readLine()
+local theme = tonumber(file.readLine())
+file.close()
 
 term.setBackgroundColor(colors.black)
 term.clear()
 term.setCursorPos(1,1)
 
+term.setTextColor(theme)
 print("STYRTA OS")
+term.setTextColor(colors.white)
 print("")
-print("Ladowanie systemu...")
+print("Witaj, "..username)
 sleep(1)
 
 shell.run("apps/launcher.lua")
